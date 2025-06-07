29f63254a941ba416cac03da6e08e14513febadd
hardiksainidelhi <hardiksainidelh@gmail.com>
2025-06-07 07:54:06 +0000

    'final2

--- script.js
+++ script.js
@@ -5,6 +5,5 @@ button.addEventListener('click',()=>{
     container.innerHTML = '<div id="kermit"><img src="./tenor.gif" width="100%"><img src="./none.gif" width="100%"></div><a id = "redirect" href="https://www.wikihow.life/Advertise-a-Marijuana-Business-Online"></a>'
     setTimeout(() => {
         const redirect = document.querySelector('#redirect')
-        redirect.click()
     }, 3000);
 })
